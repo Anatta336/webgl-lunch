e8b404ba6cb0fb81806b3b11834c197404952fc6
Sam Driver <sdriver@gmx.co.uk>
2023-10-21 12:14:08 +0100

    fix server.js deploy

--- scripts/deploy-sockets.sh
+++ scripts/deploy-sockets.sh
@@ -10,7 +10,7 @@ service_local_check=$(md5sum ./systemd/webgl-sockets.service \
 service_remote_check=$(ssh root@"$remote_ip" "md5sum /etc/systemd/system/webgl-sockets.service"\
     | awk '{ print $1 }')
 
-# rsync will do nothing if they're the same.
+# rsync will do nothing if the same file is already there.
 rsync -avz --no-perms --no-owner --no-group \
     ./systemd/webgl-sockets.service root@"$remote_ip":/etc/systemd/system/webgl-sockets.service
 
@@ -18,6 +18,9 @@ server_local_check=$(md5sum ./sockets/server.js \
     | awk '{ print $1 }')
 server_remote_check=$(ssh root@"$remote_ip" "md5sum /var/www/webgl/sockets/server.js" \
     | awk '{ print $1 }')
+
+rsync -avz --no-perms --no-owner --no-group \
+    ./sockets/server.js root@"$remote_ip":/var/www/webgl/sockets/server.js
 
 if [ "$service_local_check" != "$service_remote_check" ] \
     || [ "$server_local_check" != "$server_remote_check" ] \
